90c040e0a6565d38f7a81caa54d613210c30cc18
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2023-01-06 12:04:03 +0100

    setup useragen for mobile detection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import PortfolioMain from '../components/portfolio-main/portfolio-main'
 
-const Home: NextPage = () => {
+import { parse } from 'next-useragent'
+
+const Home: React.FC<any> = ({ userAgentString }): React.ReactElement => {
+  if (userAgentString) {
+    // Dispatch for mobile
+    console.log('Is Mobile', parse(userAgentString).isMobile)
+  } else {
+    // Dispatch for mobile
+    console.log('Is Mobile', parse(userAgentString).isMobile)
+  }
+
   return (
     <div>
       <Head>
@@ -16,6 +25,14 @@ const Home: NextPage = () => {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context: any) {
+  return {
+    props: {
+      userAgentString: context.req.headers['user-agent'],
+    },
+  }
 }
 
 export default Home
