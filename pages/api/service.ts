563a81dbee68f0bb434cc775c9ea2392c6416673
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2023-01-06 11:58:01 +0100

    Initialize next cors and api server

---
+++ pages/api/service.ts
@@ -0,0 +1,25 @@
+// to check size of objects
+export function roughSizeOfObject(object: any) {
+  const objectList = []
+  const stack = [object]
+  let bytes = 0
+
+  while (stack.length) {
+    const value = stack.pop()
+
+    if (typeof value === 'boolean') {
+      bytes += 4
+    } else if (typeof value === 'string') {
+      bytes += value.length * 2
+    } else if (typeof value === 'number') {
+      bytes += 8
+    } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+      objectList.push(value)
+
+      for (const i in value) {
+        stack.push(value[i])
+      }
+    }
+  }
+  return bytes
+}
