e1081dddf26fc2b6b0a635b459d46ba265f41225
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2023-01-07 23:28:04 +0100

    Refactor navbar imports

--- components/navBar/nav-bar-right/nav-bar-right.tsx
+++ components/navBar/nav-bar-right/nav-bar-right.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import HamburgerMenuIcon from './hamburger-menu-icon'
 
-import styles from '../../../styles/nav-bar.module.scss'
-import { useSelector } from 'react-redux'
 import contextReducerSelector from '../../../store/reducers/context-reducer/constext-reducer-selector'
+
+import styles from '../../../styles/nav-bar.module.scss'
 
 const NavBarRight = () => {
   const isMobile = useSelector(contextReducerSelector.getIsMobile)
