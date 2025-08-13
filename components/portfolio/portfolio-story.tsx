86cf8da6ae8248b4078b5ac1bfdb6cde259efa06
writeriks <emirhaktan@gmail.com>
2025-08-13 22:42:18 +0200

    Update company name

--- components/portfolio/portfolio-story.tsx
+++ components/portfolio/portfolio-story.tsx
@@ -10,7 +10,7 @@ const PortfolioStory = () => (
       <span>
         <p>
           Hi, my name is Emir. I am from Istanbul/Turkiye. I am a frontend developer. Currently i live in Warsaw/Poland
-          and work for Microstrategy Poland. I love programming, travelling, doing sport and playing computer games...
+          and work for Box Poland. I love programming, travelling, doing sport and playing computer games...
         </p>
       </span>
     </span>
